3cee2e350c560fde9470d610c5c826f11f3188a1
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-21 21:41:58 -0400

    updated homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,18 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section w-full flex-center flex-col>
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        The Prompts app is an open-source AI tool designed for the modern era,
+        enabling users to explore, create, and share creative prompts
+        effortlessly.
+      </p>
+    </section>
+  );
 };
 
 export default Home;
