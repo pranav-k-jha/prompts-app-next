33971fd3c55e10aa876c189c2b2f36392c829cb8
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-28 21:15:57 -0400

    added authentication routes and connected to mongodb

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,13 @@
+import { connectToDB } from "@utils/database";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
-console.log({clientId: process.env.GOOGLE_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET})
+import User from "@/models/user";
+
+console.log({
+  clientId: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+});
 
 const handler = NextAuth({
   providers: [
@@ -11,10 +16,36 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+    (session.user.d = sessionUser), _id.toString();
+
+    return session;
+  },
 
   async signIn({ profile }) {
-    try{}catch {}
+    try {
+      await connectToDB();
+
+      //check if user already exists
+      const userExists = await User.findOne({ email: profile.email });
+
+      //if not, create a new document and save user in MongoDB
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", " ").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log("Error checking if user exists: ", error.message);
+      return false;
+    }
   },
 });
 
