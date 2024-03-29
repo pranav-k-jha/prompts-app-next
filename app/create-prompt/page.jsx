fad117a3ced6ca33396066be844e6b0ac1f06101
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-29 12:23:43 -0400

    create post added

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React, { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
+
+const page = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "  ",
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+  };
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
+
+export default page;
