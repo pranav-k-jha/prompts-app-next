95b9bb1800ac0b9c473a1e447612d571e4ce7355
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-29 19:23:17 -0400

    updated useRouter import

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/router";
 
 import Form from "@components/Form";
 
